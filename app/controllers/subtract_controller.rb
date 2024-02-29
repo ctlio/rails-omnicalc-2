9fdb5336994583780a28d8137418eb2d91b23141
crystal tliourane <129141063+ctlio@users.noreply.github.com>
2024-02-29 18:29:20 +0000

    final commits

---
+++ app/controllers/subtract_controller.rb
@@ -0,0 +1,16 @@
+class SubtractController < ApplicationController
+
+  def show_sub_form
+    render({ :template => "subtraction_templates/subtraction_form" })
+  end
+
+  def subtract_these
+    @first_num = params.fetch("first_num").to_f
+    @second_num= params.fetch("second_num").to_f
+
+    @result =  @second_num - @first_num
+
+    render({ :template => "subtraction_templates/subtraction_results" })
+  end
+  
+end
